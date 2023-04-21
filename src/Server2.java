5eadf0b6c67eeba65568d9d91f7cb4797ca5d1b2
Joel <joho.hoffmann@gmx.de>
2023-04-21 16:48:05 +0200

    sync

--- src/Server2.java
+++ src/Server2.java
@@ -20,7 +20,7 @@ public class Server2 extends Server {
 
         //setup all partner server
         this.partnerServerList = new ArrayList<>();
-        this.partnerServerList.add(new ConnectionInetPortList("192.168.178.29", 7777));
+        this.partnerServerList.add(new ConnectionInetPortList("192.168.178.81", 7777));
         this.partnerServerList.add(new ConnectionInetPortList("192.168.178.29", 8888));
         this.partnerServerList.add(new ConnectionInetPortList("192.168.178.29", 9999));
 
